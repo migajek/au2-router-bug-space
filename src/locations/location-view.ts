a88d73086c8116adc84a85bf18c34b37715cadd0
migajek <michal.gajek@outlook.com>
2026-03-31 18:09:22 +0200

    more info

--- src/locations/location-view.ts
+++ src/locations/location-view.ts
@@ -1,11 +1,22 @@
+
+
 export class LocationView {
   private location: string;
+  hasTwoSpacesInUrl: boolean;
 
   canLoad({id}: { id: string }) {
     this.location = id;
   }
 
+  loaded(){
+    setTimeout(() => {
+      this.hasTwoSpacesInUrl = window.location.href.match(/(%20| )/g).length > 1;
+    }, 10);
+  }
+
   get isProblematicLocation(): boolean {
     return this.location.indexOf(' ') > -1;
   }
+
+  
 }
