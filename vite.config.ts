2500db7b1625ca50498949348260b6923fa0d6c3
migajek <michal.gajek@outlook.com>
2026-03-31 17:59:56 +0200

    let's try deploying it

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import aurelia from '@aurelia/vite-plugin';
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH ?? '/',
   server: {
     open: !process.env.CI,
     port: 9000,
