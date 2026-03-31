d63b29b4b4bfecfabb5998f2c0446d31c96aca30
migajek <michal.gajek@outlook.com>
2026-03-31 18:10:15 +0200

    readme and route

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
 # au2-router-space-bug-demo
 
-This project is bootstrapped by [aurelia/new](https://github.com/aurelia/new).
-
-## Quick start
-
-    npm install
-    npm start
-
-
-## Start dev web server
-
-    npm start
-
-## Build the app in production mode
-
-    npm run build
-
-
+See the demo live here https://migajek.github.io/au2-router-bug-space/
--- src/my-app.ts
+++ src/my-app.ts
@@ -6,7 +6,7 @@ import { Locations } from "./locations/locations";
 @route({
   title: "Demo",
   routes: [
-    {path: '', redirectTo: 'devices'},    
+    {path: '', redirectTo: 'locations'},    
     {path: 'devices', id: 'device-list', component: DeviceList},
     {path: 'device/:id', id: 'device', component: Device},
     {path: 'locations', id: 'locations', component: Locations},
